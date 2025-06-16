4eec561da8f06f4b128bbcfcc15656a316763964
serhiy.melnychuk <serhiy.melnychuk@backendlessmail.com>
2025-06-16 17:05:28 +0300

    update api key storage

--- src/WeatherRepo.jsx
+++ src/WeatherRepo.jsx
@@ -1,7 +1,7 @@
 import {addHistory, setWeather} from "./rootReducer";
 import axios from "axios";
 
-const API_KEY = '';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const queryWeather = (searchQuery) => {
     return async (dispatch) => {
